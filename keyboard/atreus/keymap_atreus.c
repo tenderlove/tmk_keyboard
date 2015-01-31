be072c2bb9a05665637e4c95a08748e64a0a65e0
Aaron Patterson <aaron.patterson@gmail.com>
2015-01-30 16:22:12 -0800

    updating layout for me

--- keyboard/atreus/keymap_atreus.c
+++ keyboard/atreus/keymap_atreus.c
@@ -5,14 +5,14 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* 0: Norman */
   KEYMAP(Q,    W,   D,    F,     K,               J,    U,    R,    L,    SCLN, \
          A,    S,   E,    T,     G,               Y,    N,    I,    O,    H, \
-         Z,    X,   C,    V,     B,         FN20, P,    M,    COMM, DOT, QUOT, \
-         LSFT, TAB, LALT, LGUI,  SPC,  ENT,       BSPC, FN0,  UP,   DOWN, RSFT),
+         Z,    X,   C,    V,     B,         FN20, P,    M,    COMM, DOT,  QUOT, \
+         LSFT, TAB, LALT, LGUI,  SPC,  ENT,       BSPC, FN0,  BSLS, SLSH, RSFT),
 
   /* 1: fn with software-dvorak-ized punctuation */
-  KEYMAP(1,    2,    3,    4,    5,               6,    7,    8,    9,    0, \
-         MINS, FN1,  FN2,  FN3,  RBRC,            LBRC, MINS, EQL,  FN4,  FN5, \
-         FN6,  FN7,  FN8,  FN9,  FN10,      TRNS, FN11, FN12, FN13, FN15, FN14,  \
-         FN19, FN18, TRNS, TRNS, GRV,  TRNS,      TRNS, TRNS, FN16, FN17, BSLS ) \
+  KEYMAP(FN6,  FN7,  FN4,  FN5,  FN17,            UP,   7,    8,    9,  FN8, \
+         FN8,  FN9,  FN2,  FN3,  GRV,             DOWN, 4,    5,    6,  FN1, \
+         FN10, FN11, LBRC, RBRC, FN15,      TRNS, HAT,  1,    2,    3, BSLS,  \
+         FN19, FN18, MINS, FN16, TRNS, TRNS,      TRNS, TRNS, DOT,  0, EQL ) \
 };
 
 enum function_id {
@@ -41,21 +41,21 @@ const uint16_t PROGMEM fn_actions[] = {
   [0] = ACTION_LAYER_MOMENTARY(1),  // to Fn overlay
 
   // row 1
-  [1] = ACTION_MODS_KEY(MOD_LSFT, KC_EQL), // +
-  [2] = ACTION_MODS_KEY(MOD_LSFT, KC_9),
-  [3] = ACTION_MODS_KEY(MOD_LSFT, KC_0),
+  [1] = ACTION_MODS_KEY(MOD_LSFT, KC_EQL),  // +
+  [2] = ACTION_MODS_KEY(MOD_LSFT, KC_9),    // (
+  [3] = ACTION_MODS_KEY(MOD_LSFT, KC_0),    // )
   [4] = ACTION_MODS_KEY(MOD_LSFT, KC_LBRC), // {
   [5] = ACTION_MODS_KEY(MOD_LSFT, KC_RBRC), // }
 
   // row 2
-  [6] = ACTION_MODS_KEY(MOD_LSFT, KC_1),
-  [7] = ACTION_MODS_KEY(MOD_LSFT, KC_2),
-  [8] = ACTION_MODS_KEY(MOD_LSFT, KC_3),
-  [9] = ACTION_MODS_KEY(MOD_LSFT, KC_4),
-  [10] = ACTION_MODS_KEY(MOD_LSFT, KC_5),
-  [11] = ACTION_MODS_KEY(MOD_LSFT, KC_6),
-  [12] = ACTION_MODS_KEY(MOD_LSFT, KC_7),
-  [13] = ACTION_MODS_KEY(MOD_LSFT, KC_8),
+  [6] = ACTION_MODS_KEY(MOD_LSFT, KC_1),    // !
+  [7] = ACTION_MODS_KEY(MOD_LSFT, KC_2),    // @
+  [8] = ACTION_MODS_KEY(MOD_LSFT, KC_3),    // #
+  [9] = ACTION_MODS_KEY(MOD_LSFT, KC_4),    // $
+  [10] = ACTION_MODS_KEY(MOD_LSFT, KC_5),   // %
+  [11] = ACTION_MODS_KEY(MOD_LSFT, KC_6),   // ^
+  [12] = ACTION_MODS_KEY(MOD_LSFT, KC_7),   // &
+  [13] = ACTION_MODS_KEY(MOD_LSFT, KC_8),   // *
   [14] = ACTION_MODS_KEY(MOD_LSFT, KC_SLSH), // ?
   [15] = ACTION_MODS_KEY(MOD_LSFT, KC_GRV), // ~
 
